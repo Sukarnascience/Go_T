bbd3f0c47871ec2afe9e10effc030e59051ab4f2
Sukarnascience <sukarnascience@gmail.com>
2020-10-09 15:57:56 +0530

    Test commit

--- app/src/main/java/com/example/got/MainActivity.kt
+++ app/src/main/java/com/example/got/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.got
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,7 +18,16 @@ class MainActivity : AppCompatActivity() {
             val data:TextView= findViewById(R.id.textView2)
             val Acdata:Int = (1..6).random()
             data.text="$Acdata"
+
+            val DiceImage: ImageView= findViewById(R.id.imageView2)
+            when(Acdata) {
+                1 -> DiceImage.setImageResource(R.drawable.dice_1)
+                2 -> DiceImage.setImageResource(R.drawable.dice_2)
+                3 -> DiceImage.setImageResource(R.drawable.dice_3)
+                4 -> DiceImage.setImageResource(R.drawable.dice_4)
+                5 -> DiceImage.setImageResource(R.drawable.dice_5)
+                6 -> DiceImage.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 }
-
